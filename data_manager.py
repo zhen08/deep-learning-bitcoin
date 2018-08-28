afc4a3374ec06c1ef6ebdfb980a7fc6c495b65bd
Zhen Liu <liuzhen@touchjet.com>
2018-08-29 00:31:04 +0800

    Read processed data if exists.

--- data_manager.py
+++ data_manager.py
@@ -1,25 +1,28 @@
 import datetime
-
 import pandas as pd
-
+import os
 
 # http://api.bitcoincharts.com/v1/csv/
 
 def file_processor(data_file):
-    print('Reading bitcoin market data file here: {}.'.format(data_file))
-    d = pd.read_table(data_file, sep=',', header=None, index_col=0, names=['price', 'volume'])
-    d.index = d.index.map(lambda ts: datetime.datetime.fromtimestamp(int(ts)))
-    d.index.names = ['DateTime_UTC']
-    p = pd.DataFrame(d['price'].resample('5Min').ohlc())
-    p.columns = ['price_open', 'price_high', 'price_low', 'price_close']
-    v = pd.DataFrame(d['volume'].resample('5Min').sum())
-    v.columns = ['volume']
-    p['volume'] = v['volume']
+    if os.access('data/bitcoin_coinbase_M5.csv', os.R_OK):
+        print('Reading bitcoin market data file from: data/bitcoin_coinbase_M5.csv')
+        p = pd.read_csv('data/bitcoin_coinbase_M5.csv', sep='\t',index_col='DateTime_UTC')
+    else:
+        print('Reading bitcoin market data file from: {}'.format(data_file))
+        d = pd.read_table(data_file, sep=',', header=None, index_col=0, names=['price', 'volume'])
+        d.index = d.index.map(lambda ts: datetime.datetime.fromtimestamp(int(ts)))
+        d.index.names = ['DateTime_UTC']
+        p = pd.DataFrame(d['price'].resample('5Min').ohlc())
+        p.columns = ['price_open', 'price_high', 'price_low', 'price_close']
+        v = pd.DataFrame(d['volume'].resample('5Min').sum())
+        v.columns = ['volume']
+        p['volume'] = v['volume']
 
-    # drop NaN values.
-    # for example sometimes we don't have data for like one hour in a row.
-    # So we have NaN buckets of 5Min in this particular hour.
-    # Our convention is to avoid those NaN values and drop them!
-    p = p.dropna()
-    p.to_csv('data/bitcoin_coinbase_M5.csv', sep='\t')
+        # drop NaN values.
+        # for example sometimes we don't have data for like one hour in a row.
+        # So we have NaN buckets of 5Min in this particular hour.
+        # Our convention is to avoid those NaN values and drop them!
+        p = p.dropna()
+        p.to_csv('data/bitcoin_coinbase_M5.csv', sep='\t')
     return p
